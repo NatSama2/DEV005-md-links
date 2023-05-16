67daf5db975c46cc3e7b60b6bea6197a281096f7
NatSama2 <vet.nat.c@gmail.com>
2023-05-16 12:54:13 -0400

    funcion recursiva lista

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { existsSync }  = require('node:fs');
 const fs = require('fs');
 const path = require('path');
 const colors = require('colors');
+const { log } = require('node:console');
 
 const welcomeContent = fs.readFileSync('welcome.md', 'utf-8').cyan;
 console.log(welcomeContent);
@@ -15,47 +16,77 @@ if (existsSync('Pruebas/prueba.txt'))
     console.log('La ruta no existe'.red)
   }
 
-  // Leer archivo de prueba
+   // Leer archivo de prueba
 fs.readFile('Pruebas/prueba.txt','utf8', (err, data) => {
   if (err) throw err;
   console.log(data.cyan);
 });
 
-  // validar si la ruta existe
-const rout = 'C:/Users/Natalie/Desktop/[ Nat ]/Laboratoria/3er Proyecto/DEV005-data-lovers';
+  // Rutas de prueba
+//const rout = 'C:/Users/Natalie/Desktop/[ Nat ]/Laboratoria/3er Proyecto/DEV005-data-lovers';
 //const rout = 'C:/Users/Natalie/Desktop/[ Nat ]/Documentos';
 //const rout = 'C:/Users/Natalie/Desktop/Documentoss';
-fs.stat(rout, function(err, stat) {
-  if(err == null) {
+const rout = '/Users/Natalie/Desktop/[ Nat ]/Laboratoria';
+//const rout = 'Pruebas/prueba.txt';
+ 
+// Validar si la ruta existe
+if (fs.existsSync(rout)) {
+  console.log('El directorio existe:'.green, rout);
+
+  // convertir ruta a absoluta
+  console.log('Ruta absoluta: '.yellow, path.resolve(rout));
+
+  // Verificar si la ruta es un directorio
+  const isDirectory = fs.lstatSync(rout).isDirectory();
+  if (isDirectory) {
+    // Mostrar archivos en el directorio
     const files = fs.readdirSync(rout);
-    console.log('El directorio existe:'.green, rout);
     console.log('Archivos en el directorio:'.blue, files);
+
+    // Mostrar archivos con extensión .md
+    const mdFiles = files.filter((file) => path.extname(file) === '.md');
+    if (mdFiles.length > 0) {
+      console.log('Lista de archivos con extensión .md:'.blue);
+      mdFiles.forEach((file) => {
+        console.log(file.yellow);
+      });
+    } else {
+      console.log('El directorio no contiene archivos .md'.red);
     
-    // convertir ruta a absoluta
-    console.log('Ruta absoluta: '.yellow, path.resolve(rout));
+    // Función recursiva
+    const recursive = (route) => {
+      let arrayMd = [];
 
-    // mostrar archivos con extesion .md
-    const readFiles = fs.readdirSync(rout);
-    const mdFiles = readFiles.filter(file => path.extname(file) === '.md');
-if (mdFiles.length > 0) {
-  console.log('Lista de archivos con extensión .md:'.blue);
-  mdFiles.forEach(file => {
-    console.log(file.yellow);
-  });
-} else {
-  console.log('Directorio no contiene archivos .md'.red);
-}
-
-// Si no es ruta válida 
-  } else if(err.code === 'ENOENT') {
-      console.log('El archivo no existe :c'.red);
-  } else {
-      console.log('Some other error: ', err.code);
+      if (fs.statSync(route).isFile()) {
+        arrayMd.push(route);
+      } else {
+        const elements = fs.readdirSync(route);
+        elements.forEach((element) => {
+          let newRoute = path.join(route, element);
+          if (fs.statSync(newRoute).isDirectory()) {
+            arrayMd = arrayMd.concat(recursive(newRoute));
+          } else {
+            arrayMd.push(newRoute);
+          }
+        });
+      }
+      return arrayMd.filter((file) => path.extname(file) === '.md');
+    };
+    console.log('Lista de archivos .md encontrados de forma recursiva:'.blue);
+    console.log(recursive(rout));
   }
-});
+
+  } else {
+    console.log('La ruta no es un directorio'.red);
+  }
+} else {
+  console.log('La ruta no existe :c'.red);
+}
 
 
 }, 3000);
+
+
 
 /* module.exports = () => {
   // ...
